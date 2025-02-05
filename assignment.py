074d5623f8ba0a6ef2789fe5349cd16a7a2b89d5
dylancsu <dylan.dean@csuglobal.edu>
2025-02-05 11:09:06 -0700

    init

--- assignment.py
+++ assignment.py
@@ -1,1 +1,16 @@
 
+class Engineer:
+    def __init__(self, assertion, collab, technical):
+        self.assertion = assertion
+        self.collab=collab
+        self.technical=technical
+        print(f"Step 1. Engineer created with assertion: {assertion}, collab: {collab}, and technical: {technical}")
+class SWE:
+    def __init__(self,engineer, name):
+        self.name=name
+        self.engineer=engineer
+        print(f"Step 2. Software Engineer {self.name} created with assertion: {assertion}, collab: {collab}, and technical: {technical}")
+
+for e in ["Eric Raymond", "Linus Torvalds", "John Carmack"]:
+    engineer = SWE(Engineer(True, True, True), e)
+    print(f"Step 3. {e} created as SWE with assertion: {engineer.engineer.assertion}, collab: {engineer.engineer.collab}, and technical: {engineer.engineer.technical}")
