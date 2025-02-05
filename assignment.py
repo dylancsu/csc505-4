875bb1f1fca3d7f29611c969e1fbb0f90e019a6a
dylancsu <dylan.dean@csuglobal.edu>
2025-02-05 11:10:15 -0700

    fix

--- assignment.py
+++ assignment.py
@@ -9,7 +9,7 @@ class SWE:
     def __init__(self,engineer, name):
         self.name=name
         self.engineer=engineer
-        print(f"Step 2. Software Engineer {self.name} created with assertion: {assertion}, collab: {collab}, and technical: {technical}")
+        print(f"Step 2. Software Engineer {self.name} created with assertion: {engineer.assertion}, collab: {engineer.collab}, and technical: {engineer.technical}")
 
 for e in ["Eric Raymond", "Linus Torvalds", "John Carmack"]:
     engineer = SWE(Engineer(True, True, True), e)
